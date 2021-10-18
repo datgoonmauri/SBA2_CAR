237e131a4c85279026c193cb0f23d2bf0e4ec957
datgoonmauri <mauriciomj11@gmail.com>
2021-10-18 11:35:46 -0700

    sout seperated

--- src/org/perscholas/Main.java
+++ src/org/perscholas/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Ford myFord1 = new Ford(156, 4452.0, "Black", 2005, 10);
         Ford myFord2 = new Ford(155, 5000.0, "Pink", 1998, 5);
         Car myCar = new Car(555, 56856.0, "Red");
-        System.out.printf("MySedan Price %.2f", mySedan.getSalePrice());
-        System.out.printf("MyFord1 Price %.2f", myFord1.getSalePrice());
-        System.out.printf("MyFord2 Price %.2f", myFord2.getSalePrice());
-        System.out.printf("MyCar Price %.2f", myCar.getSalePrice());
+        System.out.printf("MySedan Price %.2f\n", mySedan.getSalePrice());
+        System.out.printf("MyFord1 Price %.2f\n", myFord1.getSalePrice());
+        System.out.printf("MyFord2 Price %.2f\n", myFord2.getSalePrice());
+        System.out.printf("MyCar Price %.2f\n", myCar.getSalePrice());
 
 }}
