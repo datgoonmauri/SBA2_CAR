2b01c8f264bdc402ded95f74193585c37e2c6c6f
datgoonmauri <mauriciomj11@gmail.com>
2021-10-18 10:16:18 -0700

    init commit

---
+++ src/org/perscholas/Sedan.java
@@ -0,0 +1,19 @@
+package org.perscholas;
+
+
+public class Sedan extends Car {
+    private int length;
+
+    public Sedan(int speed, double regularPrice, String color, int length) {
+        super(speed, regularPrice, color);
+        this.length = length;
+    }
+
+    public double getSalePrice() {
+        if (length > 20) {
+            return super.getSalePrice() - (0.05 * super.getSalePrice());
+        } else {
+            return super.getSalePrice() - (0.1 * super.getSalePrice());
+        }
+    }
+}
